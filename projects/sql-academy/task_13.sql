0048af94f15ca25de8fe6abaafecaf84a74fc24b
spasov <spasov.94@yandex.ru>
2025-11-10 13:04:59 +0500

    docs: add task_13.sql

---
+++ projects/sql-academy/task_13.sql
@@ -0,0 +1,10 @@
+﻿-- Задача: Вывести имена людей, у которых есть полный тёзка среди пассажиров
+
+-- Ссылка: https://sql-academy.org/ru/trainer/tasks/13
+
+-- Сложность: ★★☆
+
+SELECT name
+FROM Passenger
+GROUP by name
+HAVING COUNT(name) > 1
