53c83cd50708140f93991b9b936a53c3a156320c
spasov <spasov.94@yandex.ru>
2025-11-10 18:43:15 +0500

    docs: add sql taask_14, task_15

---
+++ projects/sql-academy/task_14.sql
@@ -0,0 +1,11 @@
+﻿-- Задача: В какие города летал Bruce Willis Поля в результирующей таблице: town_to
+
+-- Ссылка: https://sql-academy.org/ru/trainer/tasks/14
+
+-- Сложность: ★☆☆
+
+SELECT town_to
+FROM Trip
+	JOIN Pass_in_trip ON Trip.id = Pass_in_trip.trip
+	JOIN Passenger ON Pass_in_trip.passenger = Passenger.id
+WHERE name = 'Bruce Willis'
---
+++ projects/sql-academy/task_15.sql
@@ -0,0 +1,13 @@
+﻿-- Задача: Выведите идентификатор пассажира Стив Мартин (Steve Martin) и дату и время его прилёта в Лондон (London)
+
+-- Ссылка: https://sql-academy.org/ru/trainer/tasks/15
+
+-- Сложность: ★☆☆
+
+SELECT Pass_in_trip.passenger AS id,
+	Trip.time_in
+FROM Trip
+	LEFT JOIN Pass_in_trip ON Trip.id = Pass_in_trip.trip
+	LEFT JOIN Passenger ON Pass_in_trip.passenger = Passenger.id
+WHERE name = 'Steve Martin'
+	AND town_to = 'London'
